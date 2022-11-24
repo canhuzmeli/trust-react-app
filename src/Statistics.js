a48a8d847d3ed9c303311ee2ef2a3291cd7421de
canhuzmeli <canhuzmeli@gmail.com>
2022-11-24 18:34:20 +0000

    Added eth icon and (days)

--- src/Statistics.js
+++ src/Statistics.js
@@ -22,34 +22,76 @@ export default function Statistics(json_data) {
         <Grid container spacing={2}>
           <Grid item md={3}>
             <Typography variant="subtitle1" >
-              Account Value
+              Account Value (Ξ)
             </Typography>
             <Typography variant="h2">
-              {data.account_value}
+            {String(data.account_value).substring(0,6)}
             </Typography>
           </Grid>
           <Grid item md={3}>
             <Typography variant="subtitle1" >
-              Oldest Transaction Age
+              Oldest Transaction Age (days)
             </Typography>
             <Typography variant="h2">
-            {data.oldest_transaction_age}
+            {String((parseInt(new Date() - (new Date(data.oldest_transaction_age * 1000)))/1000)/60/60/24).split('.')[0]}
             </Typography>
           </Grid>
           <Grid item md={3}>
             <Typography variant="subtitle1" >
-              Newest Transaction Age
+              Newest Transaction Age (days)
             </Typography>
             <Typography variant="h2">
-            {data.newest_transaction_age}
+            {String((parseInt(new Date() - (new Date(data.newest_transaction_age * 1000)))/1000)/60/60/24).split('.')[0]}
             </Typography>
           </Grid>
           <Grid item md={3}>
             <Typography variant="subtitle1" >
-              Highest Transaction Value
+              Highest Transaction Value (Ξ)
+            </Typography>
+            <Typography variant="h2"> 
+            {String(data.max_transaction_value).substring(0,6)}
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid container spacing={6}>
+          <Grid item md={6}>
+          </Grid>
+          <Grid item md={6}>
+          </Grid>
+          <Grid item md={6}>
+          </Grid>
+        </Grid>
+        <Grid container spacing={2}>
+          <Grid item md={3}>
+            <Typography variant="subtitle1" >
+              Median Transaction Value (Ξ)
             </Typography>
             <Typography variant="h2">
-            {data.max_transaction_value}
+            {String(data.median_value).substring(0,6)}
+            </Typography>
+          </Grid>
+          <Grid item md={3}>
+            <Typography variant="subtitle1" >
+              Unique Recepients
+            </Typography>
+            <Typography variant="h2">
+            {String(data.count_unique_to)}
+            </Typography>
+          </Grid>
+          <Grid item md={3}>
+            <Typography variant="subtitle1" >
+              Unique Funders
+            </Typography>
+            <Typography variant="h2">
+            {String(data.count_unique_from)}
+            </Typography>
+          </Grid>
+          <Grid item md={3}>
+            <Typography variant="subtitle1" >
+              Number of Transactions
+            </Typography>
+            <Typography variant="h2">
+            {String(data.count_transactions)}
             </Typography>
           </Grid>
         </Grid>
